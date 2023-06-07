38b3a8073ad00617561a421951a318ffc83a0ec6
0dev <rojase7144@gmail.com>
2023-06-07 17:10:38 -0500

    mi ultimo commit

--- app/src/main/java/singUp/SingUpActivity.kt
+++ app/src/main/java/singUp/SingUpActivity.kt
@@ -17,7 +17,10 @@ class SingUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sing_up)
 //creating spannable string from normal string
         val mText = findViewById<TextView>(R.id.terms_and_privacy)
-        val mText1 = "vienvenido a thesocialnetwork. aceptas nuestro terminos y condiciones?"
+
+        val mText1 = " vienvenido a thesocialnetwork. aceptas nuestro terminos y condiciones?"
+
+
         val mSpannableString = SpannableString(mText1)
 
 // color styles to apply on substring
@@ -25,7 +28,11 @@ class SingUpActivity : AppCompatActivity() {
 
 // appliying color styles to substrings
 
+
+
+
         mSpannableString.setSpan(mBlue,49,73,Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+
 
 //setting text to the textview
 
